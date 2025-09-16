58189d9659efe958fdf0428a145b46834a1ab01a
WyattMac <wyatt6@163.com>
2025-09-16 21:45:25 +0800

    Create SystemException.java

---
+++ wushi-common/src/main/java/fun/xianlai/wushi/common/exceptions/SystemException.java
@@ -0,0 +1,18 @@
+package fun.xianlai.wushi.common.exceptions;
+
+/**
+ * @author WyattLau
+ */
+public class SystemException extends RuntimeException {
+    public SystemException() {
+        super("未知系统错误");
+    }
+
+    public SystemException(String message) {
+        super(message);
+    }
+
+    public SystemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
