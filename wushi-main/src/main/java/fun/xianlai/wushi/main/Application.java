a297ffd0e777729b6afde829676019061ec7a031
WyattMac <wyatt6@163.com>
2025-09-16 16:07:57 +0800

    创建wushi-main模块

---
+++ wushi-main/src/main/java/fun/xianlai/wushi/main/Application.java
@@ -0,0 +1,14 @@
+package fun.xianlai.wushi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WyattLau
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
